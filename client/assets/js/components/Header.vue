1ad2db60ed6f37f6343d9093e2572fd57837c1b1
Asa Hardcastle <asa@asahardcastle.com>
2020-11-30 10:36:23 -0500

    fix typo

--- client/assets/js/components/Header.vue
+++ client/assets/js/components/Header.vue
@@ -2,7 +2,7 @@
   <section class="bg-white p-4 2xl:px-96 border-b border-gray-200">
     <div class="container flex justify-between">
       <h1 class="logo text-3xl align-middle">
-        <router-link to="/">VIllages</router-link>
+        <router-link to="/">Villages</router-link>
       </h1>
       <div class="align-middle">
         <router-link class="align-middle" to="/sign-up">Sign Up</router-link>
