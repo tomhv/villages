6d75c2ecacd879e7c990a10122cda73b210cfcbc
Tom Haskins-Vaughan <tomhv@users.noreply.github.com>
2020-12-01 21:41:03 -0500

    [server] Added Postgres db

---
+++ docker/db/initdb.d/001_user_message.sql
@@ -0,0 +1,11 @@
+CREATE TABLE user_message (
+        event_id                VARCHAR(36) NOT NULL,
+        event_type              VARCHAR(100) NOT NULL,
+        aggregate_root_id       VARCHAR(36) NOT NULL,
+        aggregate_root_version  INTEGER NOT NULL DEFAULT '1',
+        time_of_recording       TIMESTAMPTZ,
+        payload                 VARCHAR(5000) NOT NULL
+);
+
+CREATE INDEX aggregate_root_id_ix ON user_message (aggregate_root_id);
+CREATE INDEX time_of_recording_idx ON user_message (time_of_recording);
