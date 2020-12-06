e5feba5f07322ae0bd455a3466162dea1b9f75c6
Tom Haskins-Vaughan <tomhv@users.noreply.github.com>
2020-12-06 10:09:48 -0500

    [server] Set up email notification

--- server/src/Application/User/SignUpHandler.php
+++ server/src/Application/User/SignUpHandler.php
@@ -6,13 +6,18 @@ namespace App\Application\User;
 
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Symfony\Component\Notifier\Notification\Notification;
+use Symfony\Component\Notifier\NotifierInterface;
+use Symfony\Component\Notifier\Recipient\Recipient;
 
 final class SignUpHandler implements MessageHandlerInterface
 {
+    private $notifier;
     private $logger;
 
-    public function __construct(LoggerInterface $logger)
+    public function __construct(NotifierInterface $notifier, LoggerInterface $logger)
     {
+        $this->notifier = $notifier;
         $this->logger = $logger;
     }
 
@@ -23,6 +28,15 @@ final class SignUpHandler implements MessageHandlerInterface
             $signUp->username(),
             $signUp->email(),
             $signUp->userId()
+        ));
+
+        $notification = (new Notification("You've signed up!", ['email']))
+            ->content("You've signed up for Villages!")
+        ;
+
+        // Send the notification to the recipient
+        $sentMessage = $this->notifier->send($notification, new Recipient(
+            $signUp->email(),
         ));
     }
 }
